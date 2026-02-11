b60e93534139247fa48cad77ad4a33aade35cd76
snaveee <snaaaveee@gmail.com>
2026-02-11 08:47:42 +0800

    bandalan

---
+++ slugs/nav-slug.php
@@ -0,0 +1,8 @@
+<aside>
+    <ul>
+        <li><a href="index.php?section=school&page=schoolList">Schools</a></li>
+        <li><a href="index.php?section=department&page=chooseSchool">Departments</a></li>
+        <li><a href="index.php?section=program&page=programList">Programs</a></li>
+        <li><a href="index.php?section=student&page=studentList">Students</a></li>
+    </ul>
+</aside>
